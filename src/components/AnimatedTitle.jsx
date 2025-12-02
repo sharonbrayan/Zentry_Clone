f58e580df9db30f3a106b66c6b995df00f8b2c63
sharonbrayan <sharonbrayan087@gmail.com>
2025-12-02 22:01:51 +0530

    feat: implement about section with animations

---
+++ src/components/AnimatedTitle.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AnimatedTitle = () => {
+    return (
+        <div>
+            <div className='text-center mt-5 text-4xl uppercase leading-[0.8] md:text-[6rem]'>
+                Disc<b>o</b>ver the world's l<b>a</b>rgest shared adventure
+            </div>
+        </div>
+    )
+}
+
+export default AnimatedTitle
---
+++ src/components/Button.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Button = ({id,title,leftIcon,containerClass,rightIcon}) => {
+  return (
+    <button id={id} className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black ${containerClass}`}>
+        {leftIcon}
+        <span className='relative inline-flex overflow-hidden font-general uppercase text-xs'>
+            <div>{title}</div>
+        </span>
+        {rightIcon}
+    </button>
+)
+}
+
+export default Button
